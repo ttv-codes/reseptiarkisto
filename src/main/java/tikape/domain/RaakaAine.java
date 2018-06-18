60297d8ed0466dfa9260cd2acc6b82dd8e75a599
Teijo Väärä <teivaara@gmail.com>
2018-06-19 00:45:45 +0300

    v0.14

---
+++ src/main/java/tikape/domain/RaakaAine.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikape.domain;
+
+/**
+ *
+ * @author User
+ */
+public class RaakaAine {
+    
+    private Integer id;
+    private String nimi;
+    
+    public RaakaAine(Integer id, String nimi) {
+        this.id = id;
+        this.nimi = nimi;
+    }
+    
+    public Integer getId() {
+        return this.id;
+    }
+    
+    public String getNimi() {
+        return this.nimi;
+    }
+    
+}
--- src/main/java/tikape/reseptiarkisto/Main.java
+++ src/main/java/tikape/reseptiarkisto/Main.java
@@ -7,11 +7,16 @@ package tikape.reseptiarkisto;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import spark.ModelAndView;
 import spark.Spark;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import tikape.database.Database;
+import tikape.domain.RaakaAine;
 
 /**
  *
@@ -29,17 +34,59 @@ public class Main {
         String dbUrl = System.getenv("JDBC_DATABASE_URL");
         Database db = new Database(dbUrl);
         
+        
+        // Reseptilistauksen näyttö
         Spark.get("/", (req,res) -> {
             HashMap map = new HashMap<>();
 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
         
+        
+        // Raaka-ainelistauksen näyttö
         Spark.get("/raaka-aineet/", (req,res) -> {
+            List<RaakaAine> raakaAineet = new ArrayList<>();
+            
+            Connection conn = db.getConnection();
+            
+             // tee kysely
+            PreparedStatement stmt
+                    = conn.prepareStatement("SELECT id, nimi FROM RaakaAine");
+            ResultSet tulos = stmt.executeQuery();
+
+            // käsittele kyselyn tulokset
+            while (tulos.next()) {
+                String nimi = tulos.getString("nimi");
+                Integer id = tulos.getInt("id");
+                raakaAineet.add(new RaakaAine(id,nimi));
+            }
+            // sulje yhteys tietokantaan
+            conn.close();
+            
             HashMap map = new HashMap<>();
             
             return new ModelAndView(map, "raaka-aineet");
         }, new ThymeleafTemplateEngine());
+        
+        
+        // Raaka-aineen lisäys
+        Spark.post("/raaka-aineet/", (req, res) -> {
+            Connection conn = db.getConnection();
+            
+            // tee kysely
+            PreparedStatement stmt
+                    = conn.prepareStatement("INSERT INTO RaakaAine (nimi) VALUES (?)");
+            stmt.setString(1, req.queryParams("raakaAine"));
+
+            stmt.executeUpdate();
+
+            // sulje yhteys tietokantaan
+            conn.close();
+
+            res.redirect("/raaka-aineet/");
+            
+            return "";
+        });
         
     }
 }
